421542debddff01c6120ea99870199a43e0aa1a7
jihad <zoyanopicial@gmail.com>
2025-09-22 15:52:46 +0600

    problem solve

--- src/loginLogut/Singup.jsx
+++ src/loginLogut/Singup.jsx
@@ -76,7 +76,6 @@ function Signup() {
               <RadioGroup label="Gender" orientation="horizontal" className='bg-black-mood-bg-color p-2 col-span-2 rounded-2xl flex flex-row  space-x-5'>
                 <Radio value="buenos-aires">Female</Radio>
                 <Radio value="sydney" className='ml-3'>Male</Radio>
-                <Radio value="san-francisco" className='ml-3'>Custom</Radio>
               </RadioGroup>
             </div>
             <p className='text-black-mood-text-color mt-3 leading-tight'>
